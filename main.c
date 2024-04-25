6b2c8cbb998eec97ce6775a19daa901225655cab
Tushar Koundal <154369254+rex69690@users.noreply.github.com>
2024-04-25 03:54:11 +0000

    updated

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ float calculateRentalCost(struct car car, int days) {
   return car.rental * days;}
 int main ()
 {
+  //take 3 input for cars 
   struct car car1 , car2,car3;
   printf("Input car details for car1: ");
   printf("Enter carID: ");
